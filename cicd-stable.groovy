6f797e81b35aeacfba3ff64ff600138a92056744
Haritha D <harithamma.d@ibm.com>
2023-11-15 01:07:59 -0500

    added dev and stable build groovy files

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/protobufport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' ), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
