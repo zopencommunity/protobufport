e74bd60a6253d1eac4b5c06a2ef55b42e47a9c3a
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:46 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -15,9 +15,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/protobufport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/protobufport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' ), string(name: 'BUILD_LINE', value: 'DEV')]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' ), string(name: 'BUILD_LINE', value: 'DEV')]
   }
 }
--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/protobufport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/protobufport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' ), string(name: 'BUILD_LINE', value: 'STABLE')]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/protobufport.git'), string(name: 'PORT_DESCRIPTION', value: 'Protocol Buffers - Google data interchange format' ), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
